0d7dfa0932011941c4fc91364c56395ff7c08c07
Luca Barbon <luca.barbon@sanger.ac.uk>
2021-07-09 15:04:12 +0100

    Add missing licence notices

--- examples/sge_prime_editing/check_prime_a.sh
+++ examples/sge_prime_editing/check_prime_a.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
 
+########## LICENCE ##########
+# VaLiAnT
+# Copyright (C) 2020-2021 Genome Research Ltd
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#############################
+
 SCRIPT="${PWD}/../compare_exp_results.sh"
 OUT_DIR="output_a"
 EXP_OUT_DIR="${OUT_DIR}_exp"
