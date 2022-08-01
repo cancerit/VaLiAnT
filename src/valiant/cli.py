499ec10b5260263e14829cd22fc9ffac004a980b
Luca Barbon <luca.barbon@sanger.ac.uk>
2022-08-01 11:08:40 +0100

    Draft run from configuration

--- src/valiant/cli.py
+++ src/valiant/cli.py
@@ -16,16 +16,63 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #############################
 
+import json
+import logging
 import click
+from typing import Optional
 from . import __version__
-from .sge_cli import sge
-from .cdna_cli import cdna
+from .cdna_cli import cdna, run_cdna
+from .common_cli import existing_file
+from .errors import InvalidConfig
+from .models.main_config import BaseMainConfig, CDNAMainConfig, MainConfigLoader, SGEMainConfig
+from .sge_cli import run_sge, sge
 
 
-@click.group()
+def load_main_config(fp: str) -> BaseMainConfig:
+    with open(fp) as fh:
+        try:
+            config_dict = json.load(fh)
+        except json.JSONDecodeError:
+            raise InvalidConfig("not a JSON!")
+
+    return MainConfigLoader.parse_obj({
+        'config': config_dict
+    }).config
+
+
+@click.group(invoke_without_command=True)
+@click.option('-c', '--config', 'config_fp', type=existing_file, help="Configuration file path")
 @click.version_option(__version__)
-def main():
-    pass
+@click.pass_context
+def main(ctx: click.Context, config_fp: Optional[str]):
+    if ctx.invoked_subcommand is None:
+        if not config_fp:
+            raise click.UsageError("Configuration required if no subcommand is specified!")
+
+        # Load configuration
+        try:
+            config = load_main_config(config_fp)
+
+        except InvalidConfig as ex:
+            logging.critical(f"Invalid configuration: {ex.args[0]}")
+            ctx.exit(1)
+
+        except (PermissionError, FileNotFoundError) as ex:
+            logging.critical(ex)
+            ctx.exit(1)
+
+        # Run in the appropriate mode
+        if isinstance(config, SGEMainConfig):
+            run_sge(config.params, False)
+
+        elif isinstance(config, CDNAMainConfig):
+            run_cdna(config.params)
+
+        else:
+            logging.critical("Invalid configuration!")
+            ctx.exit(1)
+
+        ctx.exit(0)
 
 
 main.add_command(sge)
