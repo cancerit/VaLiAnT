0270fa33a92155977d56a9f067c64605e4c2b39f
Luca Barbon <luca.barbon@sanger.ac.uk>
2022-10-11 15:09:49 +0100

    Set maximum Python version

--- src/setup.py
+++ src/setup.py
@@ -29,7 +29,7 @@ config = {
     'author': 'Luca Barbon',
     'author_email': 'cgphelp@sanger.ac.uk',
     'version': VERSION,
-    'python_requires': '>= 3.7',
+    'python_requires': '>=3.7,<=3.9',
     'install_requires': [
         'chardet',
         'click',
