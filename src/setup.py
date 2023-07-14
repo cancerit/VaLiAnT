1a0fe66dc2ef094276a89db66ba5f44232e3809d
Luca Barbon <luca.barbon@sanger.ac.uk>
2023-07-14 14:28:45 +0100

    Set development package version

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@
 
 ########## LICENCE ##########
 # VaLiAnT
-# Copyright (C) 2020, 2021, 2022 Genome Research Ltd
+# Copyright (C) 2020, 2021, 2022, 2023 Genome Research Ltd
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -21,7 +21,7 @@
 from setuptools import setup, find_packages
 
 PACKAGE_NAME = 'valiant'
-VERSION = '3.0.0'
+VERSION = '4.0.0.dev0'
 
 config = {
     'name': PACKAGE_NAME,
