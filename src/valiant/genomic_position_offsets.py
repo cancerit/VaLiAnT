adff7e0b98c060c5c2c207bf9950667fc2ae69f2
Luca Barbon <luca.barbon@sanger.ac.uk>
2024-01-31 16:00:55 +0000

    Handle missing coordinates in lift overs

--- src/valiant/genomic_position_offsets.py
+++ src/valiant/genomic_position_offsets.py
@@ -17,10 +17,18 @@
 #############################
 
 from dataclasses import dataclass, field
-from typing import Generic
+from typing import Callable, Generic, Iterable
 
 from .uint_range import UIntRange
+from .utils import get_end
 from .variant import VariantT, sort_variants
+from .variant_group import VariantGroup
+
+
+def get_mask(length: int, value: int = 0) -> list[int]:
+    # TODO (performance): use an array instead
+
+    return [value] * length
 
 
 @dataclass(slots=True)
@@ -29,16 +37,6 @@ class PosOffset:
 
     pos: int
     offset: int
-
-
-def compute_genomic_offset(variants: list[VariantT]) -> int:
-    """Given a list of variants, get the offset they would introduce if applied"""
-
-    # TODO: check for overlapping variants (best done upstream, when filtering the full list via PyRanges)
-    return sum(
-        variant.alt_ref_delta
-        for variant in variants
-    )
 
 
 def _filter_variants_by_range(
@@ -55,14 +53,7 @@ def _filter_variants_by_range(
     return sort_variants(a) if sort else a
 
 
-def _get_alt_length(ref_length: int, variants_in_range: list[VariantT]) -> int:
-    alt_length = ref_length + compute_genomic_offset(variants_in_range)
-    if alt_length < 0:
-        raise ValueError("Negative ALT length: verify the variants!")
-    return alt_length
-
-
-def _compute_alt_offsets(ref_start: int, alt_length: int, variants_in_range: list[VariantT]) -> list[int]:
+def _compute_alt_offsets(ref_start: int, alt_length: int, alt_variants: Iterable[VariantT]) -> tuple[list[int], list[int]]:
     """
     Generate an array of position offsets to convert relative positions in ALT to
     reference positions
@@ -70,10 +61,10 @@ def _compute_alt_offsets(ref_start: int, alt_length: int, variants_in_range: lis
     The variants are assumed to be in range and sorted by position.
     """
 
-    # TODO: use an array instead (for performance)?
-    alt_offsets = [0] * alt_length
+    alt_offsets = get_mask(alt_length)
+    ins_mask = get_mask(alt_length)
     alt_offset: int = 0
-    for variant in variants_in_range:
+    for variant in alt_variants:
 
         # Filter out variants that do not add to ALT
         if variant.alt_ref_delta > 0:
@@ -81,17 +72,19 @@ def _compute_alt_offsets(ref_start: int, alt_length: int, variants_in_range: lis
 
             # Increment offsets spanned by ALT
             for i in range(variant.alt_len):
+                offset = ref_offset + i
                 alt_offset += 1
-                alt_offsets[ref_offset + i] += i
+                alt_offsets[offset] += i
+                ins_mask[offset] = 1
 
             # Set all downstream offsets (assumes the variants to be sorted by position)
             offset = ref_offset + variant.alt_len
-            alt_offsets[offset:] = [alt_offset] * (len(alt_offsets) - offset)
+            alt_offsets[offset:] = [alt_offset] * (alt_length - offset)
 
-    return alt_offsets
+    return alt_offsets, ins_mask
 
 
-def _compute_ref_offsets(variants_in_range: list[VariantT]) -> list[PosOffset]:
+def _compute_ref_offsets(ref_variants: list[VariantT]) -> list[PosOffset]:
     """
     Map sorted positions to the cumulative offsets the corresponding variants introduce
 
@@ -100,45 +93,97 @@ def _compute_ref_offsets(variants_in_range: list[VariantT]) -> list[PosOffset]:
 
     offset: int = 0
     pos_offset = []
-    for variant in variants_in_range:
+    for variant in ref_variants:
         offset += variant.alt_ref_delta
         pos_offset.append(PosOffset(variant.pos, offset))
     return pos_offset
+
+
+def _build_variant_mask(origin: int, length: int, variants: Iterable[VariantT], cond: Callable[[VariantT], bool]) -> list[int]:
+    mask = get_mask(length)
+
+    for variant in variants:
+
+        # Filter out variants that do not add to ALT
+        if cond(variant):
+            ref_offset = variant.pos - origin
+
+            # Increment offsets spanned by ALT
+            for i in range(variant.alt_len):
+                mask[ref_offset + i] = 1
+
+    return mask
+
+
+def _compute_ref_del_mask(ref_start: int, ref_length: int, ref_variants: list[VariantT]) -> list[int]:
+    return _build_variant_mask(
+        ref_start, ref_length, ref_variants, lambda x: x.alt_ref_delta < 0)
 
 
 @dataclass(slots=True)
 class GenomicPositionOffsets(Generic[VariantT]):
     ref_start: int
     ref_length: int
-    variants_in_range: list[VariantT]
+    variants_in_range: VariantGroup[VariantT]
+
+    # ALT
     _alt_length: int = field(init=False)
+
+    # REF -> ALT
     _pos_offsets: list[PosOffset] = field(init=False)
+    _ref_del_mask: list[int] = field(init=False)
+
+    # ALT -> REF
     _ins_offsets: list[int] = field(init=False)
+    _alt_ins_mask: list[int] = field(init=False)
 
     @property
     def alt_length(self) -> int:
         return self._alt_length
+
+    @property
+    def alt_end(self) -> int | None:
+        if self.alt_length == 0:
+            return None
+
+        return get_end(self.ref_start, self.alt_length)
+
+    @property
+    def alt_range(self) -> UIntRange | None:
+        if self.alt_length == 0:
+            return None
+        return UIntRange.from_length(self.ref_start, self.alt_length)
 
     def __post_init__(self) -> None:
         if self.ref_start < 0:
             raise ValueError(f"Invalid REF start {self.ref_start}!")
         if self.ref_length < 0:
             raise ValueError(f"Invalid REF length {self.ref_length}!")
-        self._alt_length = _get_alt_length(self.ref_length, self.variants_in_range)
-        self._pos_offsets = self._compute_ref_offsets()
-        self._ins_offsets = self._compute_ins_offsets()
 
-    def _compute_ins_offsets(self) -> list[int]:
+        # Compute the length of the ALT sequence
+        self._alt_length = self.variants_in_range.get_alt_length(self.ref_length)
+        if self._alt_length < 0:
+            raise ValueError("Negative ALT length: verify the variants!")
+
+        self._ref_del_mask = self._compute_del_mask()
+        self._pos_offsets = self._compute_ref_offsets()
+        self._ins_offsets, self._alt_ins_mask = self._compute_ins_offsets()
+
+    def _compute_del_mask(self) -> list[int]:
+        return _compute_ref_del_mask(
+            self.ref_start, self.ref_length, self.variants_in_range.variants)
+
+    def _compute_ins_offsets(self) -> tuple[list[int], list[int]]:
         return _compute_alt_offsets(
-            self.ref_start, self._alt_length, self.variants_in_range)
+            self.ref_start, self._alt_length, self.variants_in_range.iter_alt_variants())
 
     def _compute_ref_offsets(self) -> list[PosOffset]:
-        return _compute_ref_offsets(self.variants_in_range)
+        return _compute_ref_offsets(self.variants_in_range.variants)
 
     @classmethod
     def from_variants(cls, ref_start: int, ref_length: int, variants: list[VariantT]):
-        variants_in_range = _filter_variants_by_range(
-            ref_start, ref_length, variants, sort=True)
+        variants_in_range = VariantGroup.from_variants(_filter_variants_by_range(
+            ref_start, ref_length, variants, sort=True))
         return cls(ref_start, ref_length, variants_in_range)
 
     def get_offset(self, pos: int) -> int:
@@ -152,19 +197,65 @@ class GenomicPositionOffsets(Generic[VariantT]):
 
         return 0
 
-    def alt_to_ref_position(self, alt_pos: int) -> int:
-        if not (self.ref_start <= alt_pos < self.ref_start + self._alt_length):
+    def validate_alt_position(self, alt_pos: int) -> None:
+        if alt_pos < self.ref_start or self.alt_end is None or alt_pos <= self.alt_end:
             raise ValueError(f"Invalid ALT position {alt_pos}: out of bounds!")
+
+    def validate_alt_range(self, r: UIntRange) -> None:
+        t = self.alt_range
+        if not t or r not in t:
+            raise ValueError(f"Invalid ALT range {r}: out of bounds!")
+
+    def alt_pos_exists_in_ref(self, alt_pos: int) -> bool:
+        return self._alt_ins_mask[alt_pos] == 0
+
+    def ref_pos_exists_in_alt(self, ref_pos: int) -> bool:
+        return self._ref_del_mask[ref_pos] == 0
+
+    def _alt_to_ref_position(self, alt_pos: int) -> int:
+        """
+        Lift a position from ALT to REF
+
+        Unsafe, no boundary or insertion mask test.
+        """
 
         return alt_pos - self._ins_offsets[alt_pos - self.ref_start]
 
-    def ref_to_alt_position(self, ref_pos: int) -> int:
+    def alt_to_ref_position(self, alt_pos: int) -> int | None:
+        """Lift a position from ALT to REF"""
+
+        self.validate_alt_position(alt_pos)
+
+        if not self.alt_pos_exists_in_ref(alt_pos):
+            return None
+
+        return self._alt_to_ref_position(alt_pos)
+
+    def alt_range_to_ref_positions(self, r: UIntRange) -> list[int]:
+        """Lift a range of positions from ALT to a sequence of positions in REF"""
+
+        self.validate_alt_range(r)
+
+        return [
+            self._alt_to_ref_position(alt_pos)
+            for alt_pos in range(r.start, r.start + len(r))
+            if self.alt_pos_exists_in_ref(alt_pos)
+        ]
+
+    def ref_to_alt_position(self, ref_pos: int) -> int | None:
+        if not self.ref_pos_exists_in_alt(ref_pos):
+            return None
         return ref_pos + self.get_offset(ref_pos)
 
     def ref_to_alt_range(self, r: UIntRange) -> UIntRange:
-        return UIntRange(
-            self.ref_to_alt_position(r.start),
-            self.ref_to_alt_position(r.end))
+        # TODO: consider whether there's a requirement for this
+        #  to be able to return a range with gaps, considering
+        #  that custom variants can't overlap background variants.
+        start = self.ref_to_alt_position(r.start)
+        end = self.ref_to_alt_position(r.end)
+        assert start is not None
+        assert end is not None
+        return UIntRange(start, end)
 
     def ref_to_alt_variant(self, variant: VariantT) -> VariantT:
         return variant.clone(pos=self.ref_to_alt_position(variant.pos))
