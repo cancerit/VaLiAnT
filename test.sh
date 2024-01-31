320dcf6b7c23c2b1c149169c47888d119cf85d73
Luca Barbon <luca.barbon@sanger.ac.uk>
2024-01-31 16:04:30 +0000

    Update test script

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 PKG=src/valiant
 
-pycodestyle --ignore=E501,W504 ${PKG}
+pycodestyle --ignore=E501,W504,E266 ${PKG}
 
 stmt=$(pytest --cov=${PKG} 2>/dev/null | grep TOTAL | tr -s ' ' | cut -f2 -d' ')
 
