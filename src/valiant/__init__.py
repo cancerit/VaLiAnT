01073c2208f9687faba663571eb0566ce1982159
Luca Barbon <luca.barbon@sanger.ac.uk>
2024-03-19 18:05:47 +0000

    Bump version

--- src/valiant/__init__.py
+++ src/valiant/__init__.py
@@ -1,6 +1,6 @@
 ########## LICENCE ##########
 # VaLiAnT
-# Copyright (C) 2020, 2021, 2022, 2023 Genome Research Ltd
+# Copyright (C) 2020, 2021, 2022, 2023, 2024 Genome Research Ltd
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -16,4 +16,4 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #############################
 
-__version__ = '4.0.0.rc2'
+__version__ = '4.0.0.rc7'
