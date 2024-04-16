e49797f96e4617143ce49cafdb6adf9b3d60bf9f
Luca Barbon <luca.barbon@sanger.ac.uk>
2024-04-16 10:25:33 +0100

    Update test script

--- test.sh
+++
@@ -1,9 +0,0 @@
-PKG=src/valiant
-
-pycodestyle --ignore=E501,W504,E266 ${PKG}
-
-stmt=$(pytest --cov=${PKG} 2>/dev/null | grep TOTAL | tr -s ' ' | cut -f2 -d' ')
-
-echo "Statements: ${stmt}"
-
-pytest tests
