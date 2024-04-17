bec191781a812a62407f0b09eb8954b6d374c423
Luca Barbon <luca.barbon@sanger.ac.uk>
2024-04-17 18:19:24 +0100

    Handle VCF not found error

--- src/valiant/sge_proc.py
+++ src/valiant/sge_proc.py
@@ -172,7 +172,11 @@ def load_custom_variants(conn: Connection, vcf_manifest_fp: str, contig: str, ta
 
     # Load manifest file
     manifest = VcfManifest.load(vcf_manifest_fp)
-    manifest.test_vcf_extance()
+    try:
+        manifest.test_vcf_extance()
+    except FileNotFoundError as ex:
+        logging.critical(ex.args[0])
+        sys.exit(1)
 
     # Set positional filter
     contig_ft = ContigFilter.from_ranges(contig, targeton_ranges)
